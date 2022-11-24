823c92ca56cb52bdac6e17630c8240842fd561c5
William Chong <me@williamchong.cloud>
2022-11-24 16:07:03 +0800

    ⚡️ Make animation speed frame independent

--- nft-book/sketch.js
+++ nft-book/sketch.js
@@ -5,8 +5,9 @@ let obj;
 let bg;
 let watermark;
 
-const captureLength = 300
 
+const totalTick = 352;
+let currentTick = 0;
 
 function preload() {
   myShader = loadShader("shader.vert", "shader.frag");
@@ -23,22 +24,24 @@ function setup() {
   createCanvas(600, 800, WEBGL);
   noStroke();
 
-  randomA = random(0.001,0.01)
-  randomB = random(0.001,0.01)
-  randomC = random(0.001,0.01)
-  randomD = random(0.001,0.01)
+  randomA = random(0.001, 0.01)
+  randomB = random(0.001, 0.01)
+  randomC = random(0.001, 0.01)
+  randomD = random(0.001, 0.01)
 
   capturer.start()
 }
 
 function draw() {
-  background(0,0,0,1);
+  background(0, 0, 0, 1);
   push();
   texture(bg);
-  translate(0,0,-300);
+  translate(0, 0, -300);
   scale(1.45);
   plane(600, 800);
   pop();
+
+  currentTick = deltaTime / 16.6 + currentTick;
 
   // shader() sets the active shader with our shader
   shader(myShader);
@@ -46,31 +49,31 @@ function draw() {
   myShader.setUniform("uMatcapTexture", matcap);
 
   push()
-  translate(0,-(100));
-  rotateX(frameCount * randomA);
-  rotateZ(frameCount * randomB);
+  translate(0, -(100));
+  rotateX(currentTick * randomA);
+  rotateZ(currentTick * randomB);
   scale(0.83);
   model(obj)
   pop()
 
   push()
-  translate(0,100);
-  rotateX(frameCount * randomC);
-  rotateZ(frameCount * randomD);
+  translate(0, 100);
+  rotateX(currentTick * randomC);
+  rotateZ(currentTick * randomD);
   scale(0.83);
   model(obj)
   pop()
 
   push();
   texture(watermark);
-  translate(0,0,300);
+  translate(0, 0, 300);
   scale(0.55);
   plane(600, 800);
   pop();
 
-  if(frameCount < captureLength){
+  if (currentTick < totalTick) {
     capturer.capture(canvas);
-  }else if (frameCount === captureLength){
+  } else if (currentTick >= totalTick) {
     console.log('finished recording.');
     capturer.stop();
     capturer.save();
