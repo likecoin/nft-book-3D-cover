fd44fef0b3fe9ac5e5151370fcff1e421ce2d286
AuroraHuang22 <aurora90376@gmail.com>
2023-02-15 14:20:42 +0800

    Add utils

---
+++ nft-book/utils/uploadFiles.js
@@ -0,0 +1,47 @@
+import FormData from "form-data";
+import fetch from "node-fetch";
+import path from "path";
+import fs from "fs";
+
+const basePath = process.cwd();
+const files = fs.readdirSync(`${basePath}/output/images`);
+
+async function runUpload() {
+  for (const file of files) {
+    const formData = new FormData();
+    const fileStream = fs.createReadStream(`${basePath}/files/images/${file}`);
+    formData.append("file", fileStream);
+
+    let url = "https://api.nftport.xyz/v0/files";
+    let options = {
+      method: "POST",
+      headers: {
+        Authorization: "",
+      },
+      body: formData,
+    };
+
+    const data = await fetch(url, options)
+      .then((res) => res.json())
+      .catch((err) => console.error("error:" + err));
+
+    if (data.file_name === ".DS_Store") continue;
+
+    const fileName = path.parse(data.file_name).name;
+    const rawdata = fs.readFileSync(`${basePath}/output/json/${fileName}.json`);
+    let metaData = JSON.parse(rawdata);
+    metaData.ipfs_hash = data.ipfs_url;
+
+    //write into json
+    fs.writeFileSync(
+      `${basePath}/output/json/${fileName}.json`,
+      JSON.stringify(metaData, null, 2)
+    );
+
+    console.log(
+      `${data.file_name} uploaded to ipfs & ${fileName}.json updated!`
+    );
+  }
+}
+
+runUpload();
